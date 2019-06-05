129e7df262b3efb59438c4697ca3c969f5c8871c
Kin Salayon <kinsalayon@gmail.com>
2019-06-05 18:45:40 +1200

    Fix config and plugin name be successfully used  by xo

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const path = require('path');
 
 module.exports = {
-   configs: {
-       moxConfig: {
-           settings: {
+    configs: {
+        moxConfig: {
+            plugins: ["mox"],
+            settings: {
                'import/resolver': {
                    webpack: {
                        config: path.resolve('./webpack.conf.js')
                    }
                }
            }
-       }
-   }
+        }
+    }
 };
